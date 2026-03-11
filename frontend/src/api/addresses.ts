615930c242cae15d4b4ca8315658a173bcf6062b
sbommin <sbommin1@gmail.com>
2026-03-11 21:43:49 +0530

    Update addresses.ts

--- frontend/src/api/addresses.ts
+++ frontend/src/api/addresses.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000/api/auth/addresses/";
+const API = "https://asianbasket.ie/api/auth/addresses/";
 
 export function authHeaders() {
   const token = localStorage.getItem("access");
